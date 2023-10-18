ec2f794af922c7ff1f760a4892b5b0e27c557e72
Hugh Bromund <hughbromund@gmail.com>
2023-10-17 21:39:26 -0700

    Adding version to footer and replacing railway with GC

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,10 +8,15 @@ export default class Footer extends Component {
         <span role="img" aria-label="love">
           ❤️
         </span>{" "}
-        by Hugh Bromund
+        by Hugh Bromund in Seattle, WA
         <br />
-        <a href="https://github.com/hughbromund/hughbromund.com">Open Source Website Source Code</a> -
-        Hosted by <a href="https://railway.app/">Railway.app</a>
+        <a href="https://github.com/hughbromund/hughbromund.com">
+          Open Source Website Source Code
+        </a>{" "}
+        | Hosted by <a href="https://cloud.google.com/">Google Cloud</a> | Version:{" "}
+        {import.meta.env.REACT_APP_VERSION
+          ? process.env.REACT_APP_VERSION
+          : "Local"}
       </div>
     );
   }
